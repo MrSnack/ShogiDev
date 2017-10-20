baebe14a0f2d5c306132910c0c4ae529306db1f8
MrSnack <karstenbienert@yahoo.de>
2017-10-20 08:20:39 +0200

    Initial commit

---
+++ src/dev/shogi/TestClass.java
@@ -0,0 +1,11 @@
+package dev.shogi;
+
+/**
+ * Created by Karsten on 20.10.2017.
+ */
+public class TestClass{
+    //Hier soll mal Code stehen
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
