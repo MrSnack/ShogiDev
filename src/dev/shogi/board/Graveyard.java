55c3a424cac89398a97a5df43e5738599d29bb8e
Wepat <Wester.p@web.de>
2017-11-16 12:20:44 +0000

    Implementieren der Variablen und Methoden des Graveyard. Einfügen des Pakages controller und der gleichnamigen Klasse.

--- src/dev/shogi/board/Graveyard.java
+++ src/dev/shogi/board/Graveyard.java
@@ -1,5 +1,29 @@
 package dev.shogi.board;
 
+import dev.shogi.figures.Figure;
+
+import java.util.ArrayList;
+
 public class Graveyard {
 
+    private ArrayList<Figure> figureList;
+    private boolean isWhite;
+
+    public Graveyard(boolean isWhite){
+        figureList = new ArrayList<>();
+        this.isWhite = isWhite;
+    }
+
+    public void add(Figure figure){
+        figureList.add(figure);
+    }
+
+    public void remove(Figure figure){
+        figureList.remove(figure);
+
+    }
+
+    public ArrayList<Figure> getFigureList() {
+        return figureList;
+    }
 }
---
+++ src/dev/shogi/controller/Controller.java
@@ -0,0 +1,14 @@
+package dev.shogi.controller;
+
+import dev.shogi.board.Board;
+
+public class Controller {
+
+    private Board board;
+
+    public Controller (){
+    //    board = new Board();
+    }
+
+
+}
