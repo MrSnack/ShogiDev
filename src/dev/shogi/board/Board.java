2805976ba49ce63a9e0843fb9b68f1acac273dc5
mulchian <jannik.wolf@live.de>
2017-11-30 12:48:06 +0100

    - Board: JavaDoc ergänzt

--- src/dev/shogi/board/Board.java
+++ src/dev/shogi/board/Board.java
@@ -10,15 +10,36 @@ import java.awt.*;
  */
 public class Board extends JFrame {
 
+    /**
+     * Festlegung der maximalen Anzahl an Feldern in einer Reihe oder Spalte
+     */
     private final int BOARDSIZE = 9;
+    /**
+     * Maximale Groesse des Programmfensters
+     */
     private final Dimension BOARDSIZEMAX = new Dimension(1000, 900);
+    /**
+     * Minimale Groesse des Programmfensters
+     */
     private final Dimension BOARDSIZEMIN = new Dimension(550, 450);
 
+    /**
+     * Festlegung der Spielsteinicons - Europaeisch oder Japanisch
+     */
     private boolean isEuropeanIcon = true;
 
+    /**
+     * Panel zur Erzeugung des Spielfeldes
+     */
     private GamePanel pnlGame = new GamePanel(BOARDSIZE, this);
+    /**
+     * Panel zur Erzeugung des Menues
+     */
     private MenuPanel pnlMenu = new MenuPanel(this);
 
+    /**
+     * Methode zum Aufbau des Boards (JFrame)
+     */
     public void buildBoard() {
         this.setTitle("Shogi");
         this.setMaximumSize(BOARDSIZEMAX);
@@ -37,14 +58,27 @@ public class Board extends JFrame {
         this.setVisible(true);
     }
 
+    /**
+     * Getter für die Auswahl der Spielsteinicons
+     *
+     * @return true, wenn europaeische Spielsteinicons gewaehlt sind, ansonsten false (jap. Icons)
+     */
     public boolean isEuropeanIcon() {
         return isEuropeanIcon;
     }
 
+    /**
+     * Festlegung der Spielsteinicons
+     * @param europeanIcon - true, wenn europaeische Icons, ansonsten false (japanische Icons)
+     */
     public void setEuropeanIcon(boolean europeanIcon) {
         isEuropeanIcon = europeanIcon;
     }
 
+    /**
+     * Getter, um das GamePanel und Methoden daraus zu bekommen
+     * @return das aktuelle GamePanel
+     */
     public GamePanel getPnlGame() {
         return pnlGame;
     }
